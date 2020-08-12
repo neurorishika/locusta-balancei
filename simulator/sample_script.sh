b64ffa59a3f6fef973f1174b02904777e7b4b830
saptarshi <saptarshi.mohanta@gmail.com>
2020-08-12 15:23:51 +0530

    updated sample_script

--- simulator/sample_script.sh
+++ simulator/sample_script.sh
@@ -3,4 +3,5 @@ name=$(cat "$file")
 name="${name}_1.0"
 
 module load python/3.7
+python /home/collins/locusta-balancei/simulator/initExperiment.py '/home/collins/locusta-balancei/odors/odorA_0.odor' '/home/collins/locusta-balancei/protocol/Dur_6000_OdorDur_1000.protocol' '/home/collins/locusta-balancei/locust/Locust_B.locust' 0 $name '1.0'
 python /home/collins/locusta-balancei/simulator/initExperiment.py '/home/collins/locusta-balancei/odors/odorA_0.odor' '/home/collins/locusta-balancei/protocol/Dur_6000_OdorDur_1000.protocol' '/home/collins/locusta-balancei/locust/Locust_A.locust' 0 $name '1.0'
