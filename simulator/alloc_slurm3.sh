2fe8f2ae32e5e96dcb5f77418bda833825d992dd
technosap <saptarshi.mohanta@gmail.com>
2020-08-27 23:23:13 +0530

    update for run

--- simulator/alloc_slurm3.sh
+++ simulator/alloc_slurm3.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-#SBATCH -N 5
-#SBATCH --time=12:00:00
+#SBATCH --time=24:00:00
 #SBATCH --job-name=tensorflow
 #SBATCH --error=out/job.%J.err
 #SBATCH --output=out/job.%J.out
 #SBATCH --partition=standard
 
-srun -lN1 -r 0 script_slurmC.sh '0.0' &
-srun -lN1 -r 1 script_slurmC.sh '0.01' &
-srun -lN1 -r 2 script_slurmC.sh '0.1' &
-srun -lN1 -r 3 script_slurmC.sh '1.0' &
-srun -lN1 -r 4 script_slurmC.sh '10.0' &
+srun -N1 script_slurmC.sh '0.0' &
+srun -N1 script_slurmC.sh '0.01' &
+srun -N1 script_slurmC.sh '0.1' &
+srun -N1 script_slurmC.sh '1.0' &
+srun -N1 script_slurmC.sh '10.0' &
 wait
--- simulator/script_slurmC.sh
+++ simulator/script_slurmC.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+start=`date +%s`
 
 cd $SLURM_SUBMIT_DIR
 
@@ -7,13 +8,19 @@ name=$(cat "$file")
 name="${name}_$1"
 
 module load python/3.7
-python /home/collins/locusta-balancei/simulator/initExperiment.py '/home/collins/locusta-balancei/odors/odorC_90.odor' '/home/collins/locusta-balancei/protocol/Dur_6000_OdorDur_1000.protocol' '/home/collins/locusta-balancei/locust/Locust_B.locust' 0 $name $1
-python /home/collins/locusta-balancei/simulator/initExperiment.py '/home/collins/locusta-balancei/odors/odorC_90.odor' '/home/collins/locusta-balancei/protocol/Dur_6000_OdorDur_1000.protocol' '/home/collins/locusta-balancei/locust/Locust_B.locust' 1 $name $1
-python /home/collins/locusta-balancei/simulator/initExperiment.py '/home/collins/locusta-balancei/odors/odorC_90.odor' '/home/collins/locusta-balancei/protocol/Dur_6000_OdorDur_1000.protocol' '/home/collins/locusta-balancei/locust/Locust_B.locust' 2 $name $1
-python /home/collins/locusta-balancei/simulator/initExperiment.py '/home/collins/locusta-balancei/odors/odorC_90.odor' '/home/collins/locusta-balancei/protocol/Dur_6000_OdorDur_1000.protocol' '/home/collins/locusta-balancei/locust/Locust_B.locust' 3 $name $1
-python /home/collins/locusta-balancei/simulator/initExperiment.py '/home/collins/locusta-balancei/odors/odorC_90.odor' '/home/collins/locusta-balancei/protocol/Dur_6000_OdorDur_1000.protocol' '/home/collins/locusta-balancei/locust/Locust_B.locust' 4 $name $1
-python /home/collins/locusta-balancei/simulator/initExperiment.py '/home/collins/locusta-balancei/odors/odorC_90.odor' '/home/collins/locusta-balancei/protocol/Dur_6000_OdorDur_1000.protocol' '/home/collins/locusta-balancei/locust/Locust_B.locust' 5 $name $1
-python /home/collins/locusta-balancei/simulator/initExperiment.py '/home/collins/locusta-balancei/odors/odorC_90.odor' '/home/collins/locusta-balancei/protocol/Dur_6000_OdorDur_1000.protocol' '/home/collins/locusta-balancei/locust/Locust_B.locust' 6 $name $1
-python /home/collins/locusta-balancei/simulator/initExperiment.py '/home/collins/locusta-balancei/odors/odorC_90.odor' '/home/collins/locusta-balancei/protocol/Dur_6000_OdorDur_1000.protocol' '/home/collins/locusta-balancei/locust/Locust_B.locust' 7 $name $1
-python /home/collins/locusta-balancei/simulator/initExperiment.py '/home/collins/locusta-balancei/odors/odorC_90.odor' '/home/collins/locusta-balancei/protocol/Dur_6000_OdorDur_1000.protocol' '/home/collins/locusta-balancei/locust/Locust_B.locust' 8 $name $1
-python /home/collins/locusta-balancei/simulator/initExperiment.py '/home/collins/locusta-balancei/odors/odorC_90.odor' '/home/collins/locusta-balancei/protocol/Dur_6000_OdorDur_1000.protocol' '/home/collins/locusta-balancei/locust/Locust_B.locust' 9 $name $1
+python /home/collins/locusta-balancei/simulator/initExperiment.py '/home/collins/locusta-balancei/odors/odorC_90.odor' '/home/collins/locusta-balancei/protocol/Dur_6000_OdorDur_1000.protocol' '/home/collins/locusta-balancei/locust/Locust_B.locust' 0 $name $1 &
+python /home/collins/locusta-balancei/simulator/initExperiment.py '/home/collins/locusta-balancei/odors/odorC_90.odor' '/home/collins/locusta-balancei/protocol/Dur_6000_OdorDur_1000.protocol' '/home/collins/locusta-balancei/locust/Locust_B.locust' 1 $name $1 &
+python /home/collins/locusta-balancei/simulator/initExperiment.py '/home/collins/locusta-balancei/odors/odorC_90.odor' '/home/collins/locusta-balancei/protocol/Dur_6000_OdorDur_1000.protocol' '/home/collins/locusta-balancei/locust/Locust_B.locust' 2 $name $1 &
+python /home/collins/locusta-balancei/simulator/initExperiment.py '/home/collins/locusta-balancei/odors/odorC_90.odor' '/home/collins/locusta-balancei/protocol/Dur_6000_OdorDur_1000.protocol' '/home/collins/locusta-balancei/locust/Locust_B.locust' 3 $name $1 &
+python /home/collins/locusta-balancei/simulator/initExperiment.py '/home/collins/locusta-balancei/odors/odorC_90.odor' '/home/collins/locusta-balancei/protocol/Dur_6000_OdorDur_1000.protocol' '/home/collins/locusta-balancei/locust/Locust_B.locust' 4 $name $1 &
+python /home/collins/locusta-balancei/simulator/initExperiment.py '/home/collins/locusta-balancei/odors/odorC_90.odor' '/home/collins/locusta-balancei/protocol/Dur_6000_OdorDur_1000.protocol' '/home/collins/locusta-balancei/locust/Locust_B.locust' 5 $name $1 &
+python /home/collins/locusta-balancei/simulator/initExperiment.py '/home/collins/locusta-balancei/odors/odorC_90.odor' '/home/collins/locusta-balancei/protocol/Dur_6000_OdorDur_1000.protocol' '/home/collins/locusta-balancei/locust/Locust_B.locust' 6 $name $1 &
+python /home/collins/locusta-balancei/simulator/initExperiment.py '/home/collins/locusta-balancei/odors/odorC_90.odor' '/home/collins/locusta-balancei/protocol/Dur_6000_OdorDur_1000.protocol' '/home/collins/locusta-balancei/locust/Locust_B.locust' 7 $name $1 &
+python /home/collins/locusta-balancei/simulator/initExperiment.py '/home/collins/locusta-balancei/odors/odorC_90.odor' '/home/collins/locusta-balancei/protocol/Dur_6000_OdorDur_1000.protocol' '/home/collins/locusta-balancei/locust/Locust_B.locust' 8 $name $1 &
+python /home/collins/locusta-balancei/simulator/initExperiment.py '/home/collins/locusta-balancei/odors/odorC_90.odor' '/home/collins/locusta-balancei/protocol/Dur_6000_OdorDur_1000.protocol' '/home/collins/locusta-balancei/locust/Locust_B.locust' 9 $name $1 &
+wait
+
+end=`date +%s`
+
+runtime=$((end-start))
+echo runtime
