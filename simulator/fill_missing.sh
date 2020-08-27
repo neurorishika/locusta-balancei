273dade6e6b53cd2b59ea3646358f281261a216c
saptarshi <saptarshi.mohanta@gmail.com>
2020-08-27 19:40:35 +0530

    updated

--- simulator/fill_missing.sh
+++ simulator/fill_missing.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH -N 2
+#SBATCH -N 3
 #SBATCH --time=12:00:00
 #SBATCH --job-name=tensorflow
 #SBATCH --error=out/job.%J.err
@@ -9,5 +9,6 @@
 
 module load python/3.7
 
-srun -lN1 -r 0 python /home/collins/locusta-balancei/simulator/initExperiment.py '/home/collins/locusta-balancei/odors/odorC_90.odor' '/home/collins/locusta-balancei/protocol/Dur_6000_OdorDur_1000.protocol' '/home/collins/locusta-balancei/locust/Locust_A.locust' 9 'test_run_0.0' '0.0' &
-srun -lN1 -r 1 python /home/collins/locusta-balancei/simulator/initExperiment.py '/home/collins/locusta-balancei/odors/odorA_0.odor' '/home/collins/locusta-balancei/protocol/Dur_6000_OdorDur_1000.protocol' '/home/collins/locusta-balancei/locust/Locust_A.locust' 9 'test_run_0.0' '0.0'
+srun -lN1 -r 0 python /home/collins/locusta-balancei/simulator/initExperiment.py '/home/collins/locusta-balancei/odors/odorC_90.odor' '/home/collins/locusta-balancei/protocol/Dur_6000_OdorDur_1000.protocol' '/home/collins/locusta-balancei/locust/Locust_A.locust' 0 'test_run_1.0' '1.0' &
+srun -lN1 -r 1 python /home/collins/locusta-balancei/simulator/initExperiment.py '/home/collins/locusta-balancei/odors/odorC_90.odor' '/home/collins/locusta-balancei/protocol/Dur_6000_OdorDur_1000.protocol' '/home/collins/locusta-balancei/locust/Locust_A.locust' 1 'test_run_1.0' '1.0' &
+srun -lN1 -r 2 python /home/collins/locusta-balancei/simulator/initExperiment.py '/home/collins/locusta-balancei/odors/odorC_90.odor' '/home/collins/locusta-balancei/protocol/Dur_6000_OdorDur_1000.protocol' '/home/collins/locusta-balancei/locust/Locust_A.locust' 2 'test_run_1.0' '1.0'
