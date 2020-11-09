bb4e7489884096c2f7e12726ccd8343b42cc879d
saptarshi <saptarshi.mohanta@gmail.com>
2020-11-09 12:53:09 +0530

    update

--- simplified/scripts/trial_0_2.sh
+++ simplified/scripts/trial_0_2.sh
@@ -6,4 +6,4 @@ name=$(cat "$file")
 name="${name}_$1_$2"
 
 module load python/3.7
-python /home/collins/locusta-balancei/simplified/initExperiment.py '/home/collins/locusta-balancei/odors/odorA_0.odor' '/home/collins/locusta-balancei/protocol/Dur_6000_OdorDur_1000.protocol' '/home/collins/locusta-balancei/locust/Locust_0_2.locust' 0 $name $1 $2
+python /home/collins/locusta-balancei/simplified/initExperiment.py '/home/collins/locusta-balancei/odors/odorB_0.odor' '/home/collins/locusta-balancei/protocol/Dur_6000_OdorDur_1000.protocol' '/home/collins/locusta-balancei/locust/Locust_0_2.locust' 0 $name $1 $2
